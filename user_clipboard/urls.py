e918e5995e3ab05217e20f0d5c7bbe1883eb6f96
Venelin Stoykov <venelin@magicsolutions.bg>
2014-08-27 14:49:38 +0300

    Fix URLs

--- user_clipboard/urls.py
+++ user_clipboard/urls.py
@@ -3,8 +3,8 @@ from django.conf.urls import url
 from .views import ClipboardFileAPIView, ClipboardImageAPIView
 
 urlpatterns = [
-    url(r'^images/(?P<pk>\d+)$', ClipboardImageAPIView.as_view(), name="clipboard_images"),
-    url(r'^images/', ClipboardImageAPIView.as_view(), name="clipboard_images"),
+    url(r'^images/(?P<pk>\d+)/$', ClipboardImageAPIView.as_view(), name="clipboard_images"),
+    url(r'^images/$', ClipboardImageAPIView.as_view(), name="clipboard_images"),
     url(r'^(?P<pk>\d+)$', ClipboardFileAPIView.as_view(), name="clipboard"),
     url(r'^', ClipboardFileAPIView.as_view(), name="clipboard"),
 ]
