7784186509e41c72bcf7a4ebbd9b268b49449d35
Venelin Stoykov <venelin@magicsolutions.bg>
2014-08-27 11:42:55 +0300

    Define urlpatterns as a pure list (don't call patterns)

--- user_clipboard/urls.py
+++ user_clipboard/urls.py
@@ -1,11 +1,10 @@
-from django.conf.urls import patterns, url
+from django.conf.urls import url
 
 from .views import ClipboardFileAPIView, ClipboardImageAPIView
 
-urlpatterns = patterns(
-    '',
+urlpatterns = [
     url(r'^images/(?P<pk>\d+)$', ClipboardImageAPIView.as_view(), name="clipboard_images"),
     url(r'^images/', ClipboardImageAPIView.as_view(), name="clipboard_images"),
     url(r'^(?P<pk>\d+)$', ClipboardFileAPIView.as_view(), name="clipboard"),
     url(r'^', ClipboardFileAPIView.as_view(), name="clipboard"),
-)
+]
