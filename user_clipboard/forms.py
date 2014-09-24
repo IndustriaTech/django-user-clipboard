56e45a5146cfcde797be5cb8d3c52a1fbf874d88
Venelin Stoykov <venelin@magicsolutions.bg>
2014-09-24 15:46:54 +0300

    Create BaseClipboardForm for easy extending if needed

--- user_clipboard/forms.py
+++ user_clipboard/forms.py
@@ -3,17 +3,19 @@ from django import forms
 from .models import Clipboard
 
 
-class ClipboardFileForm(forms.ModelForm):
-    class Meta:
-        model = Clipboard
-        fields = ('file',)
-
+class BaseClipboardForm(object):
     def save(self, commit=True):
         # Delete old file before saving the new one
         if self.instance.pk:
             old_instance = self._meta.model.objects.get(pk=self.instance.pk)
             old_instance.file.delete(save=False)
-        return super(ClipboardFileForm, self).save(commit=commit)
+        return super(BaseClipboardForm, self).save(commit=commit)
+
+
+class ClipboardFileForm(BaseClipboardForm, forms.ModelForm):
+    class Meta:
+        model = Clipboard
+        fields = ('file',)
 
 
 class ClipboardImageForm(ClipboardFileForm):
