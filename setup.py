e4781716625b5accf492e73b03495c65f57df334
Venelin Stoykov <venelin@magicsolutions.bg>
2017-02-21 18:29:53 +0200

    Add south migrations in setup packages

--- setup.py
+++ setup.py
@@ -19,6 +19,7 @@ setup(
         'user_clipboard.management',
         'user_clipboard.management.commands',
         'user_clipboard.migrations',
+        'user_clipboard.south_migrations',
         'user_clipboard.utils',
     ],
     package_data={
