d4ee9b2fdf0074ab5a98f7ca31e5777f798b6603
Venelin Stoykov <venelin.stoykov@industria.tech>
2017-08-31 14:29:35 +0300

    Show date created in administration

--- user_clipboard/admin.py
+++ user_clipboard/admin.py
@@ -6,7 +6,7 @@ from .models import Clipboard
 class ClipboardAdmin(admin.ModelAdmin):
     _thumbnail = AdminThumbnail(image_field='thumbnail')
 
-    list_display = 'filename', 'user', '_thumbnail',
-    readonly_fields = 'filename', '_thumbnail',
+    list_display = 'filename', 'user', '_thumbnail', 'date_created',
+    readonly_fields = 'filename', '_thumbnail', 'date_created',
 
 admin.site.register(Clipboard, ClipboardAdmin)
